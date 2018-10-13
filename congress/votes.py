b8d94c31740822276bb5e7b75632a4a20313b324
thoo <theinhtay@gmail.com>
2018-10-12 23:15:01 -0400

    Check vote_type category and test.

--- congress/votes.py
+++ congress/votes.py
@@ -12,7 +12,7 @@ class Votes:
     def __init__(self, api_key):
         self._api_key = api_key
 
-    def get_recent_votes(self,chamber,offset=0):
+    def get_recent_votes(self, chamber, offset=0):
         """Return the 20 most recent results, sorted by date and roll call number
 
         Parameters
@@ -28,12 +28,12 @@ class Votes:
             A dictionary containing the json response
         """
 
-        client = Client(self._api_key, 
+        client = Client(self._api_key,
                         BaseParser(),
                         BaseValidator())
-        return client.get(chamber, "votes/recent.json",offset=offset)
+        return client.get(chamber, "votes/recent.json", offset=offset)
 
-    def get_rollcall_votes(self,congress,chamber,ses_no,rcall_no):
+    def get_rollcall_votes(self, congress, chamber, ses_no, rcall_no):
         """
         Return roll call votes
 
@@ -44,7 +44,8 @@ class Votes:
         chamber : str
             `house`, `senate`
         ses_no : str
-            Session number: `1` or `2`, depending on year (`1` is odd-numbered years, `2` is even-numbered years)
+            Session number: `1` or `2`, depending on year
+            `1` is odd-numbered years, `2` is even-numbered years
         rcall_no : str
             Roll call number
 
@@ -54,15 +55,15 @@ class Votes:
             A dictionary containing the json response
 
         """
-        
-        client = Client(self._api_key, 
+
+        client = Client(self._api_key,
                         BaseParser(),
                         BaseValidator())
-        return client.get(congress,chamber, 
-                          "sessions",ses_no,"votes", 
-                          rcall_no+".json")
+        return client.get(congress, chamber,
+                          "sessions", ses_no, "votes",
+                          "{}.json".format(rcall_no))
 
-    def get_votesbytype(self,congress,chamber,vote_type):
+    def get_votesbytype(self, congress, chamber, vote_type):
         """
         Return votes by type
 
@@ -78,17 +79,19 @@ class Votes:
         Returns
         -------
         pandas.DataFrame
-        """ 
-        
-        client = Client(self._api_key, 
+        """
+        if vote_type not in ['missed', 'party', 'loneno', 'perfect']:
+            raise ValueError('vote_type can only be one of [missed,party,loneno,perfect]')
+
+        client = Client(self._api_key,
                         SingleListParser("members"),
                         BaseValidator())
-        return client.get(congress,chamber,"votes",vote_type+".json") 
-    
-    def get_votesbydate(self,chamber,year,month):
+        return client.get(congress, chamber, "votes", "{}.json".format(vote_type))
+
+    def get_votesbydate(self, chamber, year, month):
         """
         Return all votes for one or both chambers in a particular month
-        
+
         Parameters
         ----------
         chamber : str
@@ -97,23 +100,23 @@ class Votes:
             YYYY format
         month : str
             MM format
-        
+
         Returns
         -------
         dict
-            A dictionary containing the json response 
+            A dictionary containing the json response
         """
-        
-        client = Client(self._api_key, 
+
+        client = Client(self._api_key,
                         BaseParser(),
                         BaseValidator())
-        return client.get(chamber,"votes", 
-                          year,month+".json") 
+        return client.get(chamber, "votes",
+                          year, "{}.json".format(month))
 
-    def get_nomination_votes(self,congress):
+    def get_nomination_votes(self, congress):
         """
         Return Senate votes on presidential nominations
-        
+
         Parameters
         ----------
         congress : str
@@ -124,42 +127,42 @@ class Votes:
         dict
             A dictionary containing the json response
         """
-        
-        client = Client(self._api_key, 
+
+        client = Client(self._api_key,
                         BaseParser(),
                         BaseValidator())
-        return client.get(congress,"nominations.json")
-        
-    def recent_expl(self,congress,offset=0):
+        return client.get(congress, "nominations.json")
+
+    def recent_expl(self, congress, offset=0):
         """
-        Return personal explanations for missed or mistaken votes in the Congressional Record 
-        
+        Return personal explanations for missed or mistaken votes in the Congressional Record
+
         Parameters
         ----------
         congress : str
-            From 107 to 115  
+            From 107 to 115
         offset : int
             This value offsets the returns of recent results
- 
+
         Returns
         -------
         dict
             A dictionary containing the json response
-        """ 
-        
-        client = Client(self._api_key, 
+        """
+
+        client = Client(self._api_key,
                         BaseParser(),
                         BaseValidator())
-        return client.get(congress,"explanations.json",offset=offset)  
+        return client.get(congress, "explanations.json", offset=offset)
 
     def recent_expl_votes(self, congress, offset=0):
         """
-        Return responses with explanations parsed to individual votes and an additional category describing the reason 
-        
+        Return explanations parsed to individual votes and an extra category for the reason
+
         Parameters
         ----------
         congress : str
-            From 110 to 115 
+            From 110 to 115
         offset : int
             This value offsets the returns of recent results
 
@@ -167,25 +170,26 @@ class Votes:
         -------
         dict
             A dictionary containing the json response
-        """ 
-        
-        client = Client(self._api_key, 
+        """
+
+        client = Client(self._api_key,
                         BaseParser(),
                         BaseValidator())
-        return client.get(congress,"explanations", 
-                          "votes.json",offset=offset)
-        
-    def recent_expl_bycat(self,congress,category,offset=0):
+        return client.get(congress, "explanations",
+                          "votes.json", offset=offset)
+
+    def recent_expl_bycat(self, congress, category, offset=0):
         """
-        Return personal explanations for missed or mistaken votes in the Congressional Record 
-        
+        Return personal explanations for missed or mistaken votes in the Congressional Record
+
         Parameters
         ----------
         congress : str
-            From 110 to 115 
+            From 110 to 115
         category : str
             "voted-incorrectly","official-business","personal"
-            See all parameters at https://projects.propublica.org/api-docs/congress-api/votes/#get-recent-personal-explanation-votes-by-category
+            See all parameters at
+            https://projects.propublica.org/api-docs/congress-api/votes/#get-recent-personal-explanation-votes-by-category
         offset : int
             This value offsets the returns of recent results
 
@@ -193,75 +197,22 @@ class Votes:
         -------
         dict
             A dictionary containing the json response
-        """ 
-        
-        client = Client(self._api_key, 
-                        BaseParser(), 
-                        BaseValidator())
-        return client.get(congress,"explanations","votes", 
-                          category+".json",offset=offset)
+        """
 
-    def recent_expl_byper(self,member_id,congress,offset=0):
+        client = Client(self._api_key,
+                        BaseParser(),
+                        BaseValidator())
+        return client.get(congress, "explanations", "votes",
+                          "{}.json".format(category), offset=offset)
+
+    def recent_expl_byper(self, member_id, congress, offset=0):
         """
         Return recent personal explanations by a specific member
-        
+
         Parameters
         ----------
         congress : str
             From 110 to 115
-        member_id : str
-            This can be retrieved from a member list request.
-        offset : int
-            This value offsets the returns of recent results 
-        
-        Returns
-        -------
-        dict
-            A dictionary containing the json response
-        """ 
-
-        client = Client(self._api_key, 
-                        BaseParser(),
-                        BaseValidator())
-        return client.get("members",member_id,"explanations", 
-                          congress+".json",offset=offset)
-    
-    def recent_expl_votes_byper(self,member_id,congress,offset=0):
-        """
-        Return recent personal explanations for missed or mistaken votes by a specific member
-        
-        Parameters
-        ----------
-        congress : str
-            From 110 to 115
-        member_id : str
-            This can be retrieved from a member list request.
-        offset : int
-            This value offsets the returns of recent results 
-        
-        Returns
-        -------
-        dict
-            A dictionary containing the json response
-        """ 
-
-        client = Client(self._api_key, 
-                        BaseParser(),
-                        BaseValidator())
-        return client.get("members",member_id,"explanations", 
-                          congress,"votes.json",offset=offset)
-        
-    def recent_expl_bycat_byper(self,congress,category,member_id,offset=0):
-        """
-        Return personal explanations for missed or mistaken votes in the Congressional Record 
-        
-        Parameters
-        ----------
-        congress : str
-            From 110 to 115 
-        category : str
-            "voted-incorrectly","official-business","personal"
-            See all parameters at https://projects.propublica.org/api-docs/congress-api/votes/#get-recent-personal-explanation-votes-by-category
         member_id : str
             This can be retrieved from a member list request.
         offset : int
@@ -271,11 +222,65 @@ class Votes:
         -------
         dict
             A dictionary containing the json response
-        """ 
-        
-        client = Client(self._api_key, 
+        """
+
+        client = Client(self._api_key,
                         BaseParser(),
                         BaseValidator())
-        return client.get("members",member_id,"explanations", 
-                          congress,"votes", 
-                          category+".json",offset=offset) 
+        return client.get("members", member_id, "explanations",
+                          "{}.json".format(congress), offset=offset)
+
+    def recent_expl_votes_byper(self, member_id, congress, offset=0):
+        """
+        Return recent personal explanations for missed or mistaken votes by a specific member
+
+        Parameters
+        ----------
+        congress : str
+            From 110 to 115
+        member_id : str
+            This can be retrieved from a member list request.
+        offset : int
+            This value offsets the returns of recent results
+
+        Returns
+        -------
+        dict
+            A dictionary containing the json response
+        """
+
+        client = Client(self._api_key,
+                        BaseParser(),
+                        BaseValidator())
+        return client.get("members", member_id, "explanations",
+                          congress, "votes.json", offset=offset)
+
+    def recent_expl_bycat_byper(self, congress, category, member_id, offset=0):
+        """
+        Return personal explanations for missed or mistaken votes in the Congressional Record
+
+        Parameters
+        ----------
+        congress : str
+            From 110 to 115
+        category : str
+            "voted-incorrectly","official-business","personal"
+            See all parameters at
+            https://projects.propublica.org/api-docs/congress-api/votes/#get-recent-personal-explanation-votes-by-category
+        member_id : str
+            This can be retrieved from a member list request.
+        offset : int
+            This value offsets the returns of recent results
+
+        Returns
+        -------
+        dict
+            A dictionary containing the json response
+        """
+
+        client = Client(self._api_key,
+                        BaseParser(),
+                        BaseValidator())
+        return client.get("members", member_id, "explanations",
+                          congress, "votes",
+                          "{}.json".format(category), offset=offset)
--- test/test_votes.py
+++ test/test_votes.py
@@ -1,8 +1,7 @@
-import pytest
-import responses
-import os
 import json
+import responses
 from congress import Votes, Client, BaseParser, BaseValidator
+import pytest
 
 @pytest.fixture
 def votes():
@@ -30,147 +29,143 @@ def test_get_recent_votes(votes, client, read_json):
     json_sample_name = 'test/api_response_samples/all_members_sample.json'
     mock_json = read_json(json_sample_name)
     # Mock API response.
-    api_url = client.build_url(chamber,"votes/recent.json?offset=0")
+    api_url = client.build_url(chamber, "votes/recent.json?offset=0")
     print(api_url)
-    responses.add(responses.GET, api_url, json=mock_json, \
-                    status=200,match_querystring=True)
+    responses.add(responses.GET, api_url, json=mock_json,
+                  status=200, match_querystring=True)
     # Test get_all_members().
     recent_votes_dict = votes.get_recent_votes(chamber)
     assert recent_votes_dict == mock_json
 
 @responses.activate
-def test_get_rollcall_votes(votes,client,read_json):
-    congress_no='115'
+def test_get_rollcall_votes(votes, client, read_json):
+    congress_no = '115'
     chamber = 'senate'
     session_no = '1'
-    roll_call_no='17'
+    roll_call_no = '17'
 
     json_sample_name = 'test/api_response_samples/get_rollcall_votes.json'
     mock_json = read_json(json_sample_name)
-    
-    api_url=client.build_url(congress_no,chamber,\
-                          "sessions",session_no,"votes",\
-                          roll_call_no+".json")
-    responses.add(responses.GET,api_url,json=mock_json,status=200)
-    rocall_votes = votes.get_rollcall_votes(congress_no,chamber,session_no,roll_call_no)
+
+    api_url = client.build_url(congress_no, chamber,
+                               "sessions", session_no, "votes",
+                               "{}.json".format(roll_call_no))
+    responses.add(responses.GET, api_url, json=mock_json, status=200)
+    rocall_votes = votes.get_rollcall_votes(congress_no, chamber, session_no, roll_call_no)
     assert rocall_votes == mock_json
 
-
 @responses.activate
-def test_get_votesbytype(votes,client,read_json):
-    congress_no='105'
+def test_get_votesbytype(votes, client, read_json):
+    congress_no = '105'
     chamber = 'senate'
     vote_type = 'party'
 
     json_sample_name = 'test/api_response_samples/get_votesbytype.json'
     mock_json = read_json(json_sample_name)
-    
-    api_url=client.build_url(congress_no,chamber,"votes",\
-                          vote_type+".json") 
+
+    api_url = client.build_url(congress_no, chamber, "votes",
+                               "{}.json".format(vote_type))
     print(api_url)
-    responses.add(responses.GET,api_url,json=mock_json,status=200)
-    votesbytype_df = votes.get_votesbytype(congress_no,chamber,vote_type)
-    assert votesbytype_df.shape == (100,10)
+    responses.add(responses.GET, api_url, json=mock_json, status=200)
+    votesbytype_df = votes.get_votesbytype(congress_no, chamber, vote_type)
+    assert votesbytype_df.shape == (100, 10)
+    with pytest.raises(ValueError):
+        votes.get_votesbytype(congress_no, chamber, 'dummy_name')
 
 @responses.activate
-def test_get_votesbydate(votes,client,read_json):
+def test_get_votesbydate(votes, client, read_json):
     chamber = 'senate'
     year = '2016'
     month = '04'
-    
+
     json_sample_name = 'test/api_response_samples/get_votesbydate.json'
     mock_json = read_json(json_sample_name)
-    
-    api_url=client.build_url(chamber,"votes",\
-                          year,month+".json")
-    responses.add(responses.GET,api_url,json=mock_json,status=200)
-    votesbydate = votes.get_votesbydate(chamber,year,month)
+
+    api_url = client.build_url(chamber, "votes",
+                               year, "{}.json".format(month))
+    responses.add(responses.GET, api_url, json=mock_json, status=200)
+    votesbydate = votes.get_votesbydate(chamber, year, month)
     assert votesbydate == mock_json
 
 @responses.activate
-def test_get_nomination_votes(votes,client,read_json):
+def test_get_nomination_votes(votes, client, read_json):
     congress_no = '110'
 
     json_sample_name = 'test/api_response_samples/get_nomination_votes.json'
     mock_json = read_json(json_sample_name)
-    
-    api_url=client.build_url(congress_no,"nominations.json")
-    responses.add(responses.GET,api_url,json=mock_json,status=200)
+
+    api_url = client.build_url(congress_no, "nominations.json")
+    responses.add(responses.GET, api_url, json=mock_json, status=200)
     nomination_votes = votes.get_nomination_votes(congress_no)
     assert nomination_votes == mock_json
 
 @responses.activate
-def test_recent_expl(votes,client,read_json):
+def test_recent_expl(votes, client, read_json):
     congress_no = '114'
 
     json_sample_name = 'test/api_response_samples/recent_expl.json'
     mock_json = read_json(json_sample_name)
-    
-    api_url=client.build_url(congress_no,"explanations.json?offset=0")
-    responses.add(responses.GET,api_url,json=mock_json,status=200)
+
+    api_url = client.build_url(congress_no, "explanations.json?offset=0")
+    responses.add(responses.GET, api_url, json=mock_json, status=200)
     explanation = votes.recent_expl(congress_no)
     assert explanation == mock_json
 
 @responses.activate
-def test_recent_expl_votes(votes,client,read_json):
+def test_recent_expl_votes(votes, client, read_json):
     congress_no = '114'
 
     json_sample_name = 'test/api_response_samples/recent_expl_votes.json'
     mock_json = read_json(json_sample_name)
-    
-    api_url=client.build_url(congress_no,"explanations",\
-                          "votes.json?offset=0")
-    responses.add(responses.GET,api_url,json=mock_json,status=200)
+
+    api_url = client.build_url(congress_no, "explanations",
+                               "votes.json?offset=0")
+    responses.add(responses.GET, api_url, json=mock_json, status=200)
     explanation = votes.recent_expl_votes(congress_no)
     assert explanation == mock_json
 
 @responses.activate
-def test_recent_expl_bycat(votes,client,read_json):
+def test_recent_expl_bycat(votes, client, read_json):
     congress_no = '115'
     category = 'voted-incorrectly'
 
     json_sample_name = 'test/api_response_samples/recent_expl_bycat.json'
     mock_json = read_json(json_sample_name)
-    
-    api_url=client.build_url(congress_no,"explanations","votes",\
-                          category+".json?offset=0")
-    responses.add(responses.GET,api_url,json=mock_json,status=200)
-    explanation = votes.recent_expl_bycat(congress_no,category)
+
+    api_url = client.build_url(congress_no, "explanations", "votes",\
+                             "{}.json?offset=0".format(category))
+    responses.add(responses.GET, api_url, json=mock_json, status=200)
+    explanation = votes.recent_expl_bycat(congress_no, category)
     assert explanation == mock_json
 
 @responses.activate
-def test_recent_expl_votes_byper(votes,client,read_json):
+def test_recent_expl_votes_byper(votes, client, read_json):
     congress_no = '115'
-    member_id = 'S001193' 
+    member_id = 'S001193'
 
     json_sample_name = 'test/api_response_samples/recent_expl_votes_byper.json'
     mock_json = read_json(json_sample_name)
-    
-    api_url=client.build_url("members",member_id,"explanations",\
-                          congress_no,"votes.json?offset=0")
-    
-    responses.add(responses.GET,api_url,json=mock_json,status=200)
-    explanation = votes.recent_expl_votes_byper(member_id,congress_no)
+
+    api_url = client.build_url("members", member_id, "explanations",
+                               congress_no, "votes.json?offset=0")
+
+    responses.add(responses.GET, api_url, json=mock_json, status=200)
+    explanation = votes.recent_expl_votes_byper(member_id, congress_no)
     assert explanation == mock_json
 
 @responses.activate
-def test_recent_expl_bycat_byper(votes,client,read_json):
+def test_recent_expl_bycat_byper(votes, client, read_json):
     congress_no = '115'
     category = 'personal'
-    member_id = 'S001193' 
+    member_id = 'S001193'
 
     json_sample_name = 'test/api_response_samples/recent_expl_bycat_byper.json'
     mock_json = read_json(json_sample_name)
-    
-    api_url=client.build_url("members",member_id,"explanations",\
-                          congress_no,"votes",\
-                          category+".json?offset=0")
+
+    api_url = client.build_url("members", member_id, "explanations",
+                               congress_no, "votes",
+                               "{}.json?offset=0".format(category))
     print(api_url)
-    responses.add(responses.GET,api_url,json=mock_json,status=200)
-    explanation = votes.recent_expl_bycat_byper(congress_no,category,member_id)
+    responses.add(responses.GET, api_url, json=mock_json, status=200)
+    explanation = votes.recent_expl_bycat_byper(congress_no, category, member_id)
     assert explanation == mock_json
-
-
-
-
-
